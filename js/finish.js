b8782239900e6ec13d2086bea7fcd43c8d974bc3
Isaac <uniqucode@gmail.com>
2022-01-30 21:40:13 -0800

    Added getting finish scores & updating final scores

--- js/finish.js
+++ js/finish.js
@@ -1,30 +1,30 @@
-// const username = document.getElementById('username');
-// const saveScoreBtn = document.getElementById('saveScoreBtn');
-// const finalScore = document.getElementById('finalScore');
-// const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-// const finalScores = JSON.parse(localStorage.getItem('finalScores')) || [];
+const finalScores = JSON.parse(localStorage.getItem('finalScores')) || [];
 
-// const MAX_HIGH_SCORES = 5;
+const MAX_HIGH_SCORES = 7;
 
-// finalScore.innerText = mostRecentScore;
+finalScore.innerText = mostRecentScore;
 
-// username.addEventListener('keyup', () => {
-//     saveScoreBtn.disabled = !username.value;
-// });
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
 
-// saveFinalScore = (e) => {
-//     e.preventDefault();
+saveFinalScore = (e) => {
+    e.preventDefault();
 
-//     const score = {
-//         score: mostRecentScore,
-//         name: username.value,
-//     };
-//     finalScores.push(score);
-//     finalScores.sort((a, b) => b.score - a.score); // SORTING THE FINAL SCORE FROM HIGHEST TO LOWEST
-//     finalScores.splice(7);
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    finalScores.push(score);
+    finalScores.sort((a, b) => b.score - a.score); // SORTING THE FINAL SCORE FROM HIGHEST TO LOWEST
+    finalScores.splice(7);
 
-// // UPDATING THE FINAL SCORE IN LOCAL STORAGE
-//     localStorage.setItem('finalScores', JSON.stringify(finalScores));
-//     window.location.assign('/finalscores.html');
-// };
+// UPDATING THE FINAL SCORE IN LOCAL STORAGE
+    localStorage.setItem('finalScores', JSON.stringify(finalScores));
+    window.location.assign('/finalscores.html');
+};
